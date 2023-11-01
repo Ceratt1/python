a4e96d304874b6d3d9476731a457910cea807dcb
Ceratt1 <gabrielceratticabral@gmail.com>
2023-11-01 11:10:07 -0300

    testes herança

--- aulas ivonei/heranca.py
+++ aulas ivonei/heranca.py
@@ -2,13 +2,21 @@ class pessoa:
     def __init__(self, nome, idade) -> None:
         self.nome = nome
         self.idade = idade
-
-
-
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__} : {', '.join([f'{chave} = {valor}' for chave, valor in self.__dict__.items()])})"
+    def mudarNome(self, nome):
+        self.nome =nome
 class jardineiro(pessoa):
     def __init__(self, nome, idade) -> None:
         super().__init__(nome, idade)
     def mostrar(self):
         pass
+    
 
-jardineiro = jardineiro("gabriel", 19)
+
+
+a = jardineiro("gabriel", 18)
+
+a.mudarNome("joao")
+
+print (a)
