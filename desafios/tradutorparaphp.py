b78c202932bff36bd5e75e92b45baab490c17849
Gabriel Ceratti Cabral <gabrielceratticabral@gmail.com>
2023-12-10 02:55:38 -0300

    11

---
+++ desafios/tradutorparaphp.py
@@ -0,0 +1,18 @@
+lista2 = ["gabriel", "joao", "pedro"]
+
+
+
+nome = "pedro"
+
+ok = False
+
+
+while True:
+    for x in lista2:
+        if nome == x:
+            print("eyy!")
+            break
+
+        else:
+             print ("hii")
+        
